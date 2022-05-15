71017cd1d317b3f6c4e8067ba00b31801a3ab49a
rafzxc <tolentino.rafael.d.1751@gmail.com>
2022-05-15 23:26:36 +0800

    Profile only

--- src/pages/student/Profile.js
+++ src/pages/student/Profile.js
@@ -1,10 +1,41 @@
 import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import {
+  Flex,
+  Container,
+  Grid,
+  GridItem,
+  Center,
+  SimpleGrid,
+  Heading,
+  Box,
+  Stack,
+  HStack,
+  VStack,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  useColorModeValue,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+} from "@chakra-ui/react";
 
 import StudentLayout from "../../components/student/StudentLayout";
 
 import AuthContext from "../../context/auth-context";
 import useAxios from "../../utils/axios";
-
+import { Image } from '@chakra-ui/react'
+import { Button} from '@chakra-ui/react'
 const Profile = () => {
   const { user } = useContext(AuthContext);
   const [userInfo, setUserInfo] = useState("");
@@ -35,12 +66,89 @@ const Profile = () => {
       });
   }, []);
 
-  return (
+  return(
     <StudentLayout>
-      {userInfo.first_name}
+           {userInfo.first_name}
     {userInfo.last_name}
     {studentInfo.student_id}
-    </StudentLayout>
+      <Stack m={5} spacing={5}>
+        <Box
+          bg="white"
+          borderRadius="10px"
+          box-shadow="md"
+          padding={10}
+          lineHeight={1.25}
+          height="110vh"
+        >
+    <SimpleGrid minChildWidth="300px" spacing={5} lineHeight={1.25} paddingBottom={40} >
+      <Box bg="#EDF2F7" borderRadius="10px" box-shadow="md"  padding={10}>
+        <Flex justifyContent="center">
+          <Image
+            borderRadius='full'
+            boxSize='400px'
+            src='https://bit.ly/dan-abramov'
+            alt='Dan Abramov'
+        />
+        </Flex>
+        <Flex justifyContent="center">
+        <Stack direction={['row']}spacing='100px'>
+              <Box ><Button colorScheme='yellow'>Update<br></br>Details</Button></Box>
+              <Box ><Button colorScheme='yellow'>Change<br></br>Password</Button></Box>
+              </Stack>
+              </Flex>
+
+      </Box>
+      <Box bg="#EDF2F7" borderRadius="10px" box-shadow="md"  padding={10}>
+               <SimpleGrid columns={2} spacingX='40px' spacingY='20px' >
+                <Text as='b'>First Name</Text>
+                <FormControl id="firstName">
+                  <FormLabel>N/A</FormLabel>
+                </FormControl>
+
+                <Text as='b'>Last Name</Text>
+                <FormControl id="lastname">
+                  <FormLabel>N/A</FormLabel>
+                </FormControl>
+
+                <Text as='b'>Middle Name</Text>
+                <FormControl id="middlename">
+                  <FormLabel>N/A</FormLabel>
+                </FormControl>
+
+                <Text as='b'>Year Level</Text>
+                <FormControl id="yearlevel">
+                  <FormLabel>N/A</FormLabel>
+                </FormControl>
+
+                <Text as='b'>Section</Text>
+                <FormControl id="section">
+                  <FormLabel>N/A</FormLabel>
+                </FormControl>
+
+                <Text as='b'>Student Number</Text>
+                <FormControl id="studnum">
+                  <FormLabel>N/A</FormLabel>
+                </FormControl>
+
+                <Text as='b'>Email Adress</Text>
+                <FormControl id="emailadd">
+                  <FormLabel>N/A</FormLabel>
+                </FormControl>
+
+                <Text as='b'>Home Adress</Text>
+                <FormControl id="homeadd">
+                  <FormLabel>N/A</FormLabel>
+                </FormControl>
+               </SimpleGrid>
+               
+      </Box>
+      
+    </SimpleGrid>
+    
+    <hr></hr>
+      </Box>
+       </Stack>   
+    </StudentLayout> 
   );
 };
 
